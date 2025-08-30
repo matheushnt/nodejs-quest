bdf02cece918c635f1e0f04cffc97eb143db96fe
Matheus Dev <matheushnt.dev@gmail.com>
2025-08-30 14:25:18 -0300

    Consumindo uma stream completa

--- streams/fake-upload-to-http-stream.js
+++ streams/fake-upload-to-http-stream.js
@@ -7,14 +7,14 @@ class OneToHundredStream extends Readable {
     const i = this.index++;
 
     setTimeout(() => {
-      if (i > 100) {
+      if (i > 5) {
         this.push(null);
       } else {
         const buf = Buffer.from(String(i));
 
         this.push(buf);
       }
-    }, 150);
+    }, 300);
   }
 }
 
@@ -22,4 +22,6 @@ fetch('http://localhost:3334', {
   method: 'POST',
   body: new OneToHundredStream(),
   duplex: 'half',
-});
+})
+  .then((response) => response.text())
+  .then((data) => console.log(data));
