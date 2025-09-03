8930312b55ab3041410d58e2ad8e16b6729b55ff
Matheus Dev <matheushnt.dev@gmail.com>
2025-09-03 14:30:00 -0300

    Capturando query parameters

--- src/server.js
+++ src/server.js
@@ -1,18 +1,7 @@
 import http from 'node:http';
 import { json } from './middlewares/json.js';
 import { routes } from './routes.js';
-
-/*
-Formas de enviar informações para o back-end:
- - Query Parameters: parâmetros nomeados enviados na URL da requisição. Caract.: URL Stateful. Usados como filtros, paginação, ou seja, modificam a resposta. Não são obrigatórios.
-   - Ex.: http://localhost:3333/users?userId=1&name=Matheus
- - Route Parameters: parâmetros não nomeados enviados também na URL da requisição. São usados para Identificação de Recursos.
-   - Ex.: GET http://localhost:3333/users/1
-   - Ex.: DELETE http://localhost:3333/users/1
- - Request Body: Envio de informações, geralmente de formulários. Caso seja utilizado HTTPS, as infos são mais difíceis de serem violadas.
-   - Ex.: POST http://localhost:3333/users/
-          -> os dados são enviados no body
-*/
+import { extractQueryParams } from '../utils/extract-query-params.js';
 
 const server = http.createServer(async (req, res) => {
   const { method, url } = req;
@@ -26,7 +15,10 @@ const server = http.createServer(async (req, res) => {
   if (route) {
     const routeParams = req.url.match(route.path);
 
-    req.params = { ...routeParams.groups };
+    const { query, ...params } = routeParams.groups;
+
+    req.params = params;
+    req.query = query ? extractQueryParams(query) : {};
 
     return route.handler(req, res);
   }
