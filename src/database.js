693efbe509712468ef897c7c6759b77c8fdef782
Matheus Dev <matheushnt.dev@gmail.com>
2025-09-03 14:53:18 -0300

    Filtrando lista do banco de dados

--- src/database.js
+++ src/database.js
@@ -19,8 +19,18 @@ export class Database {
     fs.writeFile(databasePath, JSON.stringify(this.#database));
   }
 
-  select(table) {
-    const data = this.#database[table] ?? [];
+  select(table, search) {
+    let data = this.#database[table] ?? [];
+
+    // { name: 'Matheus', email: 'Matheus' }
+    // [['name', 'Matheus'], ['email', 'Matheus']]
+    if (search) {
+      data = data.filter((row) => {
+        return Object.entries(search).some(([key, value]) => {
+          return row[key].toLowerCase().includes(value.toLowerCase());
+        });
+      });
+    }
 
     return data;
   }
